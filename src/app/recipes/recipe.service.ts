92898907b149db050b8b936ac92f147317bb081f
Agustín Jiménez Fernández <ajifernandez@macbook-pro-de-agustin.home>
2019-02-19 16:33:03 +0000

    Using a service to get the recipes

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,20 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg"
+    ),
+    new Recipe(
+      "Another Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg"
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); //return a new array -> copy
+  }
+}
